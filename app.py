b9d558fa9866e9d5b788c68de335a51148e2571b
BrunaFelema <brunafelema3@gmail.com>
2026-04-02 16:39:12 -0300

    Update title and add category selection

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import requests
 
-st.title("Enviar para Google Sheets 🚀")
+st.title("Cadastro 🚀")
 
 mensagem = st.text_input("Digite uma mensagem:")
+
+categoria = st.selectbox(
+    "Escolha uma categoria:",
+    ["Trabalho", "Estudo", "Pessoal"]
+)
 
 if st.button("Enviar"):
     url = "https://docs.google.com/forms/d/e/1FAIpQLSdwYdIOIYzZPJRQLspIA_rqx-C4XvhasGVaDksuuaGn--QLuQ/formResponse"
 
     dados = {
-        "entry.377580072": mensagem
+        "entry.377580072": mensagem,
+        "entry.1339358369": categoria
     }
 
     resposta = requests.post(url, data=dados)
