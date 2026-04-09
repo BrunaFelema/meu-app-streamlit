8a2e7089051d328e7f6701dcff37f626b3e66787
BrunaFelema <brunafelema3@gmail.com>
2026-04-09 15:51:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,70 @@
 import streamlit as st
-import requests
+import urllib.parse
+import urllib.request
+import json
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+import io
 
 st.title("Cadastro 🚀")
 
 mensagem = st.text_input("Digite uma mensagem:")
+categoria = st.selectbox("Escolha uma categoria:", ["Trabalho", "Estudo", "Pessoal"])
 
-categoria = st.selectbox(
-    "Escolha uma categoria:",
-    ["Trabalho", "Estudo", "Pessoal"]
-)
+imagem = st.file_uploader("Envie uma imagem", type=["png", "jpg", "jpeg"])
 
 if st.button("Enviar"):
-    url = "https://docs.google.com/forms/d/e/1FAIpQLSdwYdIOIYzZPJRQLspIA_rqx-C4XvhasGVaDksuuaGn--QLuQ/formResponse"
+
+    # ================= GOOGLE DRIVE =================
+    creds_dict = st.secrets["gcp_service_account"]
+    creds = service_account.Credentials.from_service_account_info(creds_dict)
+
+    drive_service = build("drive", "v3", credentials=creds)
+
+    file_id = ""
+
+    if imagem is not None:
+        file_bytes = io.BytesIO(imagem.read())
+
+        file_metadata = {
+            "name": imagem.name,
+            "parents": ["1kCwwzZbZ-eruwRtoAESgjbTAYoCFa5pU"]
+        }
+
+        media = MediaIoBaseUpload(file_bytes, mimetype=imagem.type)
+
+        file = drive_service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields="id"
+        ).execute()
+
+        file_id = file.get("id")
+
+        # deixa público
+        drive_service.permissions().create(
+            fileId=file_id,
+            body={"role": "reader", "type": "anyone"}
+        ).execute()
+
+        link_imagem = f"https://drive.google.com/uc?id={file_id}"
+
+    else:
+        link_imagem = ""
+
+    # ================= GOOGLE FORMS =================
+    url = "SEU_LINK_FORM"
 
     dados = {
-        "entry.377580072": mensagem,
-        "entry.1339358369": categoria
+    "entry.1984707711": mensagem,
+    "entry.377580072": categoria,
+    "entry.1339358369": link_imagem
+
     }
 
-    resposta = requests.post(url, data=dados)
+    url_final = url + "?" + urllib.parse.urlencode(dados)
 
-    if resposta.status_code == 200:
-        st.success("Enviado com sucesso!")
-    else:
-        st.error("Erro ao enviar!")
+    urllib.request.urlopen(url_final)
+
+    st.success("Enviado com sucesso!")
